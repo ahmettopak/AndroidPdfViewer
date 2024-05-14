e9a9478268d89c0ee876d0ce5156f48bed3d154a
Ahmet TOPAK <75997899+ahmettopak@users.noreply.github.com>
2024-05-14 10:22:45 +0300

    Upload project

---
+++ app/src/main/java/com/ahmet/androidpdfviewer/MainActivity.java
@@ -0,0 +1,21 @@
+package com.ahmet.androidpdfviewer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.github.barteksc.pdfviewer.PDFView;
+
+public class MainActivity extends AppCompatActivity {
+    private PDFView pdfView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        pdfView = findViewById(R.id.pdfView);
+
+        pdfView.fromAsset("example.pdf")
+                .load();
+    }
+}
