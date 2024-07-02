68b69fa6ebe1929a3bb72305b9d0251c5a73d46c
Ahmet TOPAK <75997899+ahmettopak@users.noreply.github.com>
2024-07-02 17:33:57 +0300

    Nat manuel add.

--- app/src/main/java/com/ahmet/androidpdfviewer/MainActivity.java
+++ app/src/main/java/com/ahmet/androidpdfviewer/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         pdfView = findViewById(R.id.pdfView);
 
-        pdfView.fromAsset("userManuel.pdf")
+        pdfView.fromAsset("nat.pdf")
                 .load();
     }
 }
